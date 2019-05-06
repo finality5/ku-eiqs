d3278a1986c2204e18550305743d5ab38f31378d
Tanakorn Prangsriarun <tanagon601@gmail.com>
2019-05-07 00:49:41 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/databaseFrontend/precache-manifest.a912d7e305ec12f65564734e13441073.js"
+  "/ku-eiqs/precache-manifest.c8fd93929be7c1f1843b15ee119660b1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/databaseFrontend/index.html", {
+workbox.routing.registerNavigationRoute("/ku-eiqs/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
